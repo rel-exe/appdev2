8262f8a631034afab0b30611d568848906706dd4
Your Name <you@example.com>
2026-04-17 16:48:08 +0800

    Done with using the navigation object section

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
+import { createStaticNavigation,  useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Link } from '@react-navigation/native';
 import { Button } from '@react-navigation/elements';
 
+
 function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Link screen="Details" >Go to Details</Link>
-      <Button screen="Details">Go to Details</Button
+      <Button onPress={() => navigation.navigate('Details')}>
+        Go to Details
+      </Button>
     </View>
   );
 }
+
 
 function DetailsScreen() {
   return (
@@ -24,7 +29,7 @@ function DetailsScreen() {
 }
 
 const RootStack = createNativeStackNavigator({
-   initialRouteName: 'Home',
+  initialRouteName: 'Home',
   screenOptions: {
     headerStyle: { backgroundColor: 'tomato' },
   },
